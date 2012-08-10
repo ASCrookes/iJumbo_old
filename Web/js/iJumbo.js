69c93d5379edf9d0abf06a47c3e9a17a87b9809b
Amadou Crookes <Amadou.Crookes@Tufts.edu>
2012-08-09 23:39:55 -0400

    updated itunes screenshots and added correct sketch project(UI)

---
+++ Web/js/iJumbo.js
@@ -0,0 +1,11 @@
+function iosClick() {
+	alert("iOS App Coming Soon!");
+}
+
+function submitFeedback() {
+    alert("Thanks for the feedback!\nWe really appreciate it.");
+}
+
+function fbClick() {
+	alert("Facebook Page Coming Soon!");
+}
