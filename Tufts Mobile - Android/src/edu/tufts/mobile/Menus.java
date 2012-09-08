34c7c10e6a31fb92538ba3e588be79202bf52218
Jake Rosenberg <jake@myemailstop.com>
2012-09-08 14:48:41 -0400

    All Android
    This contains all of the files to date of the Android project.

---
+++ Tufts Mobile - Android/src/edu/tufts/mobile/Menus.java
@@ -0,0 +1,14 @@
+package edu.tufts.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Menus extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tm_menus);
+	}
+	
+}
